06c8ecfb3ffc1b1a06ec2f16c7ff8745dbae59a5
Slava <kuncovs19@gmail.com>
2024-01-22 07:53:26 +0300

    fix rebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoCollectionComponent } from './components/todo-collection/todo-collection.component';
+import { TodoGroupComponent } from './components/todo-group/todo-group.component';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	standalone: true,
+	templateUrl: './app.component.html',
+	imports: [
+		CommonModule,
+		RouterOutlet,
+		TodoGroupComponent,
+		TodoCollectionComponent,
+	],
+	providers: [BrowserAnimationsModule, BrowserModule],
 })
-export class AppComponent {
-  title = 'angular-todo';
+export class AppComponent implements OnInit {
+	constructor(private primengConfig: PrimeNGConfig) {}
+
+	ngOnInit() {
+		this.primengConfig.ripple = true;
+		this.primengConfig.zIndex = {
+			modal: 1100, // dialog, sidebar
+			overlay: 1000, // dropdown, overlaypanel
+			menu: 1000, // overlay menus
+			tooltip: 1100, // tooltip
+		};
+	}
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {
+	BrowserAnimationsModule,
+	provideAnimations,
+} from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
-
+import { PrimeNGConfig } from 'primeng/api';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+	providers: [
+		provideRouter(routes),
+		importProvidersFrom(PrimeNGConfig),
+		BrowserModule,
+		BrowserAnimationsModule,
+		provideAnimations(),
+		provideAnimations(),
+	],
 };
